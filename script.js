9d600df924ab2b6279a271c3d92f74481ec257a1
Staś <83009119+neworldheh@users.noreply.github.com>
2024-03-31 09:44:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 const body = document.querySelector("body")
 const menu = document.querySelector("div.menu")
 const info = document.querySelector("div.info")
-const first = document.querySelector("div.first")
-const second = document.querySelector("div.second")
 const back = document.querySelector("button")
 const img2 = document.querySelector("img.img_logo2")
 const select = document.querySelector("div.select")
 const option = ["afryka", "ameryka_pol", "ameryka_pul", "antarktyda", "australia", "eu", "azja"];
-const content = [afryka = document.querySelector("div.afrykac"), ameryka_pol = document.querySelector("div.ameryka_polc"),
-ameryka_pul = document.querySelector("div.ameryka_pulc"),
-antarktyda = document.querySelector("div.antarktydac"),
-australia = document.querySelector("div.australiac"),
-eu = document.querySelector("div.euc"),
-azja = document.querySelector("div.azjac")];
 const bcgk = document.querySelector("source")
 const vid = document.querySelector("video")
+const br = document.querySelector("div.br")
 const selected = document.getElementsByTagName("a");
-let cont;
 let clicked
 let click
 back.remove()
@@ -30,14 +22,13 @@ for(let i = 1; i <= 7; i++){
         console.log(selected[clicked])
         const hove = selected[clicked]
         click = selected[clicked]
-        cont = content[clicked-1]
         for(n = 7;n>=1;n--){
             selected[n].remove();
-            content[n-1].remove();
         }
         body.append(img2)
         select.append(back)
         select.appendChild(click)
+        br.remove()
         menu.remove()
         select.classList.remove("select")
         hove.classList.remove(option[clicked - 1])
@@ -46,37 +37,7 @@ for(let i = 1; i <= 7; i++){
             location.reload();
         })
         vid.remove()
-        if(i === 1){
-            body.style.backgroundImage = "url('afryka.jpg')"
-            body.append(info)
-            window.addEventListener("scroll", () => {
-                let scroll = window.pageYOffset;
-                    let speed = info.dataset.speed
-                    info.style.transform = `translateY(${scroll * speed}px)`
-            })
-            info.appendChild(cont)
-        }
-        else if(i === 2){
-            body.style.backgroundImage = "url('ameryka_pol.jpg')"
-            body.append(info)
-            window.addEventListener("scroll", () => {
-                let scroll = window.pageYOffset;
-                    let speed = info.dataset.speed
-                    info.style.transform = `translateY(${scroll * speed}px)`
-            })
-            info.appendChild(cont)
-        }
-        else if(i === 3){
-            body.style.backgroundImage = "url('ameryka_pul.jpg')"
-            body.append(info)
-            window.addEventListener("scroll", () => {
-                let scroll = window.pageYOffset;
-                    let speed = info.dataset.speed
-                    info.style.transform = `translateY(${scroll * speed}px)`
-            })
-            info.appendChild(cont)
-        }
-        else if(i === 4){
+        if(i === 4){
             body.style.backgroundImage = "url('antarktyda.jpg')"
             body.append(info)
             window.addEventListener("scroll", () => {
@@ -84,37 +45,6 @@ for(let i = 1; i <= 7; i++){
                     let speed = info.dataset.speed
                     info.style.transform = `translateY(${scroll * speed}px)`
             })
-            info.appendChild(cont)
-        }
-        else if(i === 5){
-            body.style.backgroundImage = "url('australia.jpg')"
-            body.append(info)
-            window.addEventListener("scroll", () => {
-                let scroll = window.pageYOffset;
-                    let speed = info.dataset.speed
-                    info.style.transform = `translateY(${scroll * speed}px)`
-            })
-            info.appendChild(cont)
-        }
-        else if(i === 6){
-            body.style.backgroundImage = "url('eu.jpg')"
-            body.append(info)
-            window.addEventListener("scroll", () => {
-                let scroll = window.pageYOffset;
-                    let speed = info.dataset.speed
-                    info.style.transform = `translateY(${scroll * speed}px)`
-            })
-            info.appendChild(cont)
-        }
-        else if(i === 7){
-            body.style.backgroundImage = "url('azja.jpg')"
-            body.append(info)
-            window.addEventListener("scroll", () => {
-                let scroll = window.pageYOffset;
-                    let speed = info.dataset.speed
-                    info.style.transform = `translateY(${scroll * speed}px)`
-            })
-            info.appendChild(cont)
         }
     });
 }
